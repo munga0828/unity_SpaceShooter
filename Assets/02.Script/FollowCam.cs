0105d745994df490a70f7ac364205f716ed99bbf
HP-KHJ\ccctv <munga0828@naver.com>
2017-12-08 13:28:34 +0900

    Camera Translate 전

---
+++ Assets/02.Script/FollowCam.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCam : MonoBehaviour {
+
+    public Transform targetTr;
+    public float dist = 10.0f;
+    public float height = 3.0f;
+    public float dampTrace = 20.0f;
+
+    private Transform tr;
+
+	// Use this for initialization
+	void Start () {
+        tr = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    private void LateUpdate()
+    {
+        tr.position = Vector3
+    }
+}
---
+++ Assets/02.Script/PlayerCtrl.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCtrl : MonoBehaviour {
+
+    float h = 0.0f;
+    float v = 0.0f;
+
+    Transform tr;
+
+    public float moveSpeed = 10.0f;
+
+    public float rotSpeed = 100.0f;
+
+
+
+	// Use this for initialization
+	void Start () {
+        tr = GetComponent<Transform>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        //Debug.Log(string.Format("H = {0}", h));
+        //Debug.Log(string.Format("V = {0}", v));
+
+        Vector3 movedir = (Vector3.forward * v) + (Vector3.right * h);
+
+        tr.Translate(movedir * Time.deltaTime * moveSpeed, Space.Self);
+        tr.Rotate(Vector3.up * Time.deltaTime * rotSpeed * Input.GetAxis("Mouse X"));
+
+        Debug.Log(string.Format("mouse : {0}", Input.GetAxis("Mouse X")));
+
+
+    }
+}
